b32356d70b5cc0d72a872b48c858e709072409df
Charls Darren Marfil <charlsdarrenmarfil@gmail.com>
2025-08-05 23:12:12 +0800

    added lesson 2: variables and types

--- log1.py
+++ log1.py
@@ -1,3 +1,18 @@
-
 #simple printing
-print (1+1)
+print ("hello World")
+
+
+#testing variables
+
+height = 1.8
+intro = "My name is Charls"
+x = 3
+y = 4
+z = x + y
+
+print(height)
+print(type(height))
+
+print(intro)
+print(x)
+print(type(x))
