00bd3e3ecca2625726a2115efcc5afe786e620a7
Charls Darren Marfil <charlsdarrenmarfil@gmail.com>
2025-08-05 23:37:52 +0800

    add python version, and few more variable experiments

--- log1.py
+++ log1.py
@@ -1,9 +1,12 @@
+import sys
+
+#testing python version
+print(sys.version)
+
 #simple printing
 print ("hello World")
 
-
 #testing variables
-
 height = 1.8
 intro = "My name is Charls"
 x = 3
@@ -16,3 +19,19 @@ print(type(height))
 print(intro)
 print(x)
 print(type(x))
+
+a = str(3)
+b = int(1.0)
+c = float(1)
+
+print(a)
+print(type(a))
+print(b)
+print(c)
+
+color = ["red", "green" ,"blue"]
+l, m, n = color # this is called unpacking
+
+print(l , ", " , m , ", " , n)
+
+
