7cf1c18f59acaa45848acbe53d4ac00a62ee590a
Charls Darren Marfil <charlsdarrenmarfil@gmail.com>
2025-08-07 23:32:53 +0800

    took 10 minutes to figure that last bit LOL

--- log3.py
+++ log3.py
@@ -1,3 +1,5 @@
+#while loops
+""""
 guess_number = 7
 chances = 0
 limit = 3
@@ -12,8 +14,22 @@ while chances < limit:
     chances += 1
 else:
     print("You failed")
-    
+"""
+#for loops
+"""""
+for x in range(5):
+    for y in range(5):
+        print(f"{x}, {y}")
+"""
 
+numbers = [5,2,5,2,2]
+
+for x in numbers:
+    output = ""
+    for y in range(x):
+        output += "x"
+    print(output)
+  
      
     
     
